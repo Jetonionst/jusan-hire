d9ecc9af87df34f9cc05cc67dc59f0d48d6cc354
yernazarov <zhandos.yernazarov@nu.edu.kz>
2022-07-08 17:02:46 +0600

    Add anketa dto

--- back-end/src/main/java/kz/jusan/backend/dto/AnketaDto.java
+++ back-end/src/main/java/kz/jusan/backend/dto/AnketaDto.java
@@ -8,6 +8,8 @@ import java.util.Date;
 @Data
 public class AnketaDto {
     @NotBlank
+    private String iin;
+    @NotBlank
     private String fio;
     private String previousName;
     @NotBlank
@@ -35,5 +37,47 @@ public class AnketaDto {
     private String relativeLevel;
     @NotBlank
     private String email;
-
+//    @NotBlank
+//    private String permanentCity;
+//    @NotBlank
+//    private String permanentRegion;
+//    @NotBlank
+//    private String permanentDistrict;
+//    @NotBlank
+//    private String permanentStreet;
+//    @NotBlank
+//    private String permanentHouse;
+//    private String permanentCorpus;
+//    private String permanentApartment;
+//    private boolean isAddressMatches = false;
+//    private String factualCity;
+//    private String factualRegion;
+//    private String factualDistrict;
+//    private String factualStreet;
+//    private String factualHouse;
+//    private String factualCorpus;
+//    private String factualApartment;
+//    private List<Education> educationList;
+//    private List<Extracurricular> extracurricularList;
+//    private List<Workplace> lastThreeWorkplaces;
+//    private List<RecommendationPerson> threeRecommendationPeople;
+//    private String marriageStatus;
+//    private List<Child> chilrenList;
+//    private List<Relative> relativeList;
+//    private List<CommercialOrganisation> commercialOrganisationList;
+//    private boolean isRelativeJusanEmployee;
+//    private List<RelativeJusanEmployee> relativeJusanEmployeeList;
+//    private boolean isCarOwner;
+//    private List<Car> carList;
+//    private boolean isMilitary;
+//    private boolean isSVC;
+//    private String svc;
+//    private boolean isExpiredLoan;
+//    private boolean isCriminal;
+//    private boolean isRelativeCriminal;
+//    private boolean isCriminalDelo;
+//    private boolean isAlimentPayer;
+//    private boolean isHooligan;
+//    private String additionalInfo;
+//    private boolean isExtraIncome;
 }
