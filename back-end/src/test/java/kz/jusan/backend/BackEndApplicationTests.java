d84f8b10081c5db09db61987bee23e2ffe6a789a
yernazarov <zhandos.yernazarov@nu.edu.kz>
2022-07-08 12:07:30 +0600

    Create Back-end project

---
+++ back-end/src/test/java/kz/jusan/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.jusan.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
