9cd87cfa4e53f7ef372645984c47196c7d707f55
yernazarov <zhandos.yernazarov@nu.edu.kz>
2022-07-08 18:07:58 +0600

    Minor fixes

--- back-end/src/main/java/kz/jusan/backend/controller/AuthController.java
+++ back-end/src/main/java/kz/jusan/backend/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -17,6 +18,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api/v1")
 public class AuthController {
     @Autowired
     private UserService userService;
