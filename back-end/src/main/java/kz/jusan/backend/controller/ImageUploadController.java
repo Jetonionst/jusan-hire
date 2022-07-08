44a25f524c94070e46df13ff21ce88350bbd44fb
makhambet.torezhan <makhambet.torezhan@nu.edu.kz>
2022-07-08 19:01:01 +0600

    add image upload controller

---
+++ back-end/src/main/java/kz/jusan/backend/controller/ImageUploadController.java
@@ -0,0 +1,35 @@
+package kz.jusan.backend.controller;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+
+@RestController
+@RequestMapping(path="/api/v1/upload")
+public class ImageUploadController {
+
+    @PostMapping(value = "/image", produces = {MediaType.IMAGE_JPEG_VALUE, "application/json"})
+    public String uploadImage(@RequestParam("image") MultipartFile file) throws Exception {
+        System.out.println(file.getOriginalFilename());
+        System.out.println(file.getName());
+        System.out.println(file.getContentType());
+        System.out.println(file.getSize());
+
+        String imgPath = new ClassPathResource("static/image").getFile().getAbsolutePath();
+        Files.copy(file.getInputStream(), Paths.get(imgPath+ File.separator + file.getOriginalFilename()),
+                StandardCopyOption.REPLACE_EXISTING);
+        return "Success";
+
+
+    }
+}
