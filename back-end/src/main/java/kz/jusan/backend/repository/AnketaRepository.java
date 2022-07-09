b568fbbe3c4dafff9b97cf51ffeaf36b4a208b4e
makhambet.torezhan <makhambet.torezhan@nu.edu.kz>
2022-07-09 15:33:17 +0600

    add find and delete methods

--- back-end/src/main/java/kz/jusan/backend/repository/AnketaRepository.java
+++ back-end/src/main/java/kz/jusan/backend/repository/AnketaRepository.java
@@ -2,8 +2,12 @@ package kz.jusan.backend.repository;
 
 import kz.jusan.backend.entity.AnketaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnketaRepository extends JpaRepository<AnketaEntity, String> {
+    public AnketaEntity findAnketaEntityByIin(String iin);
+
+    public void deleteByIin(String iin);
 }
