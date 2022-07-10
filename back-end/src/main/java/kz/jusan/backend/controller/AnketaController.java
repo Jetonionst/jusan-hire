a482566a5d5b48ac5aae940aab94f6f762a2544a
yernazarov <zhandos.yernazarov@nu.edu.kz>
2022-07-10 16:42:44 +0600

    add download pdf endpoint

--- back-end/src/main/java/kz/jusan/backend/controller/AnketaController.java
+++ back-end/src/main/java/kz/jusan/backend/controller/AnketaController.java
@@ -1,14 +1,18 @@
 package kz.jusan.backend.controller;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
 import kz.jusan.backend.dto.AnketaDto;
 import kz.jusan.backend.entity.AnketaEntity;
 import kz.jusan.backend.service.AnketaService;
+import kz.jusan.backend.service.PdfGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.io.FileOutputStream;
 import java.util.List;
 
 @RestController
@@ -41,6 +45,22 @@ public class AnketaController {
     @Transactional
     public void deleteAllAnketas() {
         anketaService.deleteAllAnketas();
+    }
+
+    @GetMapping("/download-pdf/{iin}")
+    public void generatePdf(@PathVariable("iin") String iin){
+        try {
+            PdfGenerator pdfGenerator = new PdfGenerator();
+            Document document = new Document();
+            AnketaEntity anketa = anketaService.findAnketaByIIN(iin);
+            PdfWriter.getInstance(document, new FileOutputStream(pdfGenerator.FILE));
+            document.open();
+            pdfGenerator.addMetaData(document, anketa);
+            pdfGenerator.addContent(document, anketa);
+            document.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
